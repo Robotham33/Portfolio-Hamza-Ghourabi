853f69002850089b740f711a4481b799a009bd97
Robotham33 <ghourabihamza92@gmail.com>
2026-09-18 16:04:04 +0200

    content: rename medical domain

--- content/home.ts
+++ content/home.ts
@@ -8,7 +8,7 @@ export const proofPoints = [
   {
     value: "02",
     label: "Domaines techniques",
-    detail: "MedTech + automobile",
+    detail: "Médical + automobile",
   },
   {
     value: "CI/CD",
