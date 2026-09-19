991f43715b07c7ae789af2cc7b687947a19208a7
Robotham33 <ghourabihamza92@gmail.com>
2026-09-19 02:00:28 +0200

    fix(metadata): generate browser icon from HGH asset

---
+++ app/icon.tsx
@@ -0,0 +1,37 @@
+/* eslint-disable @next/next/no-img-element */
+import { ImageResponse } from "next/og";
+
+export const size = {
+  width: 64,
+  height: 64,
+};
+
+export const contentType = "image/png";
+
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          background: "#ffffff",
+        }}
+      >
+        <img
+          src="https://www.hamzaghourabi.fr/hgh-favicon-v1.png"
+          alt=""
+          width={64}
+          height={64}
+          style={{
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+          }}
+        />
+      </div>
+    ),
+    size,
+  );
+}
